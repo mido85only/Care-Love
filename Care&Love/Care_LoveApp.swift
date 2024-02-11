cc78bdd0bd1d38ad2301b1fe3c770a40d52f08ad
eli-mone <elimone.tech@gmail.com>
2024-02-11 23:51:51 +0200

    Initial Commit

---
+++ Care&Love/Care_LoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Care_LoveApp.swift
+//  Care&Love
+//
+//  Created by mohammed on 11/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Care_LoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
